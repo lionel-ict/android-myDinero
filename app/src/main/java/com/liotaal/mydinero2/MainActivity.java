95d730096b7b1d2228b486761fea88aaa35e5c43
lionel-ict <lionel.ta@gmail.com>
2020-05-24 22:37:47 +0200

    Added buttons for dollar and pound change rates. Change rates saved in local files (persistence).

--- app/src/main/java/com/liotaal/mydinero2/MainActivity.java
+++ app/src/main/java/com/liotaal/mydinero2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.liotaal.mydinero2;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,13 +19,20 @@ import android.content.Context;
 import com.google.android.material.snackbar.Snackbar;
 import android.view.Menu;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText mInput;
     private TextView mTextEuro, mTextDolar, mTextLibra;
     private Button mBotonEuro, mBotonDolar, mBotonLibra;
     private Button mBotonPort;
-    private ProgressBar mBar;
+    private Button mBotonChdolar, mBotonChlibra;
+    private EditText mInputChdolar, mInputChlibra;
+    //private ProgressBar mBar;
     private ImageView mImagen;
 
     @Override
@@ -35,7 +43,9 @@ public class MainActivity extends AppCompatActivity {
         // Automatic orientation
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
 
-        // Visual components of the interface
+        ////////////////////////
+        // INTERFACE COMPONENTS
+
         mInput = (EditText) findViewById(R.id.editDinero);
         mTextEuro = (TextView) findViewById(R.id.textEuro);
         mTextDolar = (TextView) findViewById(R.id.textDolar);
@@ -44,10 +54,15 @@ public class MainActivity extends AppCompatActivity {
         mBotonDolar = (Button) findViewById(R.id.botonDolar);
         mBotonLibra = (Button) findViewById(R.id.botonLibra);
         mBotonPort = (Button) findViewById(R.id.botonPort);
-        mBar = (ProgressBar) findViewById(R.id.barra);
+        mBotonChdolar = (Button) findViewById(R.id.botonChdolar);
+        mBotonChlibra = (Button) findViewById(R.id.botonChlibra);
+        mInputChdolar = (EditText) findViewById(R.id.editChdolar);
+        mInputChlibra = (EditText) findViewById(R.id.editChlibra);
+        //mBar = (ProgressBar) findViewById(R.id.barra);
         mImagen = (ImageView) findViewById(R.id.imagen);
 
-        // Listeners and events
+        ////////////////////////
+        // LISTENERS AND EVENTS
 
         mBotonEuro.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
@@ -58,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
                 else{
                     mTextEuro.setText(String.format("%1$,.2f",Double.parseDouble(String.valueOf(mInput.getText())) /
                             166.386) + "€");
-                    mBar.setVisibility(View.VISIBLE);
+                    //mBar.setVisibility(View.VISIBLE);
                     hideKeyboard();
                 }
             }
@@ -71,9 +86,10 @@ public class MainActivity extends AppCompatActivity {
                     mostrarSnack(arg0, "ERROR Dolar: Introduzca pts");
                 }
                 else{
+                    double dolar = Double.valueOf(mInputChdolar.getText().toString()); //0.93
                     mTextDolar.setText(String.format("%1$,.2f",Double.parseDouble(String.valueOf(mInput.getText()))
-                            / 166.386 / 0.93) + "$");
-                    mBar.setVisibility(View.VISIBLE);
+                            / 166.386 / dolar) + "$");
+                    //mBar.setVisibility(View.VISIBLE);
                     hideKeyboard();
                 }
             }
@@ -86,9 +102,10 @@ public class MainActivity extends AppCompatActivity {
                     mostrarSnack(arg0, "ERROR Libra: Introduzca pts");
                 }
                 else{
+                    double libra = Double.valueOf(mInputChlibra.getText().toString()); //1.14
                     mTextLibra.setText(String.format("%1$,.2f",Double.parseDouble(String.valueOf(mInput.getText())) /
-                            166.386 / 1.14) + "£");
-                    mBar.setVisibility(View.VISIBLE);
+                            166.386 / libra) + "£");
+                    //mBar.setVisibility(View.VISIBLE);
                     hideKeyboard();
                 }
             }
@@ -102,6 +119,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mBotonChdolar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                // Save Euro-Dolar change to file dolar.daT
+                guardar_dolar();
+            }
+        });
+
+        mBotonChlibra.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                // Save Euro-Libra change to file libra.dat
+                guardar_libra();
+            }
+        });
 
         mImagen.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
@@ -109,13 +139,18 @@ public class MainActivity extends AppCompatActivity {
                 mTextEuro.setText("");
                 mTextDolar.setText("");
                 mTextLibra.setText("");
-                mBar.setVisibility(View.INVISIBLE);
+                //mBar.setVisibility(View.INVISIBLE);
                 hideKeyboard();
 
                 // Automatic orientation
                 setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
             }
         });
+
+        ////////////////////////
+        // LOAD CHDOLAR & CHLIBRA FROM FILES
+        carga_dolar();
+        carga_libra();
     }
 
     @Override
@@ -124,6 +159,8 @@ public class MainActivity extends AppCompatActivity {
         savedInstanceState.putString("EURO", mTextEuro.getText().toString());
         savedInstanceState.putString("DOLAR", mTextDolar.getText().toString());
         savedInstanceState.putString("LIBRA", mTextLibra.getText().toString());
+        savedInstanceState.putString("CHDOLAR", mInputChdolar.getText().toString());
+        savedInstanceState.putString("CHLIBRA", mInputChlibra.getText().toString());
         super.onSaveInstanceState(savedInstanceState);
     }
     @Override
@@ -133,6 +170,8 @@ public class MainActivity extends AppCompatActivity {
         mTextEuro.setText(savedInstanceState.getString("EURO"));
         mTextDolar.setText(savedInstanceState.getString("DOLAR"));
         mTextLibra.setText(savedInstanceState.getString("LIBRA"));
+        mInputChdolar.setText(savedInstanceState.getString("CHDOLAR"));
+        mInputChlibra.setText(savedInstanceState.getString("CHLIBRA"));
     }
 
     @Override
@@ -165,4 +204,80 @@ public class MainActivity extends AppCompatActivity {
                 getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(mInput.getWindowToken(), 0);
     }
+
+    private boolean existe_archivo(String[] archivos, String archbusca) {
+        for (int f = 0; f < archivos.length; f++)
+            if (archbusca.equals(archivos[f]))
+                return true;
+        return false;
+    }
+
+    private void guardar_dolar() {
+        try {
+            OutputStreamWriter archivo = new OutputStreamWriter(openFileOutput("dolar.dat",
+                    Activity.MODE_PRIVATE));
+            archivo.write(mInputChdolar.getText().toString());
+            archivo.flush();
+            archivo.close();
+        } catch (IOException e) {
+            Toast.makeText(this, "Error guardando dólar", Toast.LENGTH_SHORT).show();
+        }
+        Toast.makeText(this, "Dólar guardado", Toast.LENGTH_SHORT).show();
+    }
+
+    private void guardar_libra() {
+        try {
+            OutputStreamWriter archivo = new OutputStreamWriter(openFileOutput("libra.dat",
+                    Activity.MODE_PRIVATE));
+            archivo.write(mInputChlibra.getText().toString());
+            archivo.flush();
+            archivo.close();
+        } catch (IOException e) {
+            Toast.makeText(this, "Error guardando libra", Toast.LENGTH_SHORT).show();
+        }
+        Toast.makeText(this, "Libra guardado", Toast.LENGTH_SHORT).show();
+    }
+
+    private void carga_dolar() {
+        String[] archivos = fileList();
+        if (existe_archivo(archivos, "dolar.dat")) {
+            try {
+                InputStreamReader archivo = new InputStreamReader(openFileInput("dolar.dat"));
+                BufferedReader br = new BufferedReader(archivo);
+                String linea = br.readLine();
+                String todas = "";
+                while (linea != null) {
+                    todas = todas + linea + "\n";
+                    linea = br.readLine();
+                }
+                br.close();
+                archivo.close();
+                mInputChdolar.setText(todas);
+            } catch (IOException e) {
+                Toast.makeText(this, "Error leyendo dólar", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void carga_libra() {
+        String[] archivos = fileList();
+        if (existe_archivo(archivos, "libra.dat")) {
+            try {
+                InputStreamReader archivo = new InputStreamReader(openFileInput("libra.dat"));
+                BufferedReader br = new BufferedReader(archivo);
+                String linea = br.readLine();
+                String todas = "";
+                while (linea != null) {
+                    todas = todas + linea + "\n";
+                    linea = br.readLine();
+                }
+                br.close();
+                archivo.close();
+                mInputChlibra.setText(todas);
+            } catch (IOException e) {
+                Toast.makeText(this, "Error leyendo libra", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
